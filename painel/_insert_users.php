7d2bb6f7c8c80c24bd4c2bea6edf416ba5a48cd0
VitorPalermo <101677215+VitorPalermo@users.noreply.github.com>
2022-09-05 22:27:18 -0300

    Initial commit

---
+++ painel/_insert_users.php
@@ -0,0 +1,28 @@
+<?php
+include'./conexao/conexao.php';
+// include './script/password.php';
+
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+$nivel = 3;
+
+$sql = "SELECT FROM * usuario WHERE mail = '$mail'";
+$search = mysqli_query($conexao,$sql);
+
+$total = mysqli_num_rows($search);  
+// conta quantas linhas temos no resultado
+
+if($total > 0){ 
+    header('location: ..//index.php?msg=3');
+    // mensagem de retorno
+
+}else{
+
+    $sql = "INSERT INTO usuario (mail,password,id_users_nivel)
+    values ('$mail' sha1('$password'),$nivel)";
+    $insert = mysqli_query($conexao, $sql);
+
+    header('location: form_users.php?msg=1');
+}
+
+
